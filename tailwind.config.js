54a9b87983066fa6f56f371dd76c8b6627065dff
YasinAkturk <yasnaktrk@gmail.com>
2022-07-27 00:20:44 +0300

    TailwindCss yapısı eklendi

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    screens: {
+      'xs': '480px',
+    },
+    extend: {
+      colors: {
+        'yenigri': '#484848',
+        'acikgri': '#999999',
+        'footer': '#1A1A1A'
+      },
+    },
+  },
+  plugins: [],
+}
