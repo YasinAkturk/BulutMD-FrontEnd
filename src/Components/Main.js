3500fd6504751639326116eb79e258819ce6ab71
YasinAkturk <yasnaktrk@gmail.com>
2022-07-27 12:34:35 +0300

    Film ve Dizi başlıklarına görsel eklendi

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,16 +1,19 @@
+import DiziFilm from "../../Movie.json"
 export default function Main() {
+    let veri=DiziFilm;
     return (
+        
         <div className="flex flex-wrap mt-10 xs:justify-start justify-center">
             <div className="flex flex-col items-center mb-10 xs:ml-10 ">
-                <div className="bg-red-400 h-60 w-60 rounded-2xl">
-                    <a href=""><img></img></a>
-                </div>
+                
+                    <a href=""><img className="h-60 w-60 rounded-2xl" src={veri.entries[98].images["Poster Art"].url}></img></a>
+                
                 <p className="text-2xl font-normal mt-4">Film</p>
             </div>
             <div className="flex flex-col items-center xs:ml-10">
-                <div className="bg-gray-400 h-60 w-60 rounded-2xl">
-                    <a href=""><img></img></a>
-                </div>
+                
+                <a href=""><img className="h-60 w-60 rounded-2xl" src={veri.entries[0].images["Poster Art"].url}></img></a>
+                
                 <p className="text-2xl font-normal mt-4">Dizi</p>
             </div>
         </div>
