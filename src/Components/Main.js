fe6694f0fdccdb23f5f287ea551ef4e7b1dab0a0
YasinAkturk <yasnaktrk@gmail.com>
2022-07-27 20:27:24 +0300

    fazlalık olan kodlar silindi

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,9 +2,7 @@ import { Link } from "react-router-dom";
 import Movies from "../Components/Movies"
 import Series from "../Components/Series"
 import { veri } from "../data"
-export default function Main() {
-    var imgurl = [];
-    
+export default function Main() {    
     
     var myObj = (veri.map(data => data.images["Poster Art"].url));
     var myJSON = JSON.stringify(myObj);
@@ -16,9 +14,6 @@ export default function Main() {
     return (
 
         <div className="flex flex-wrap mt-10 xs:justify-start justify-center">
-
-            <p>{imgurl}</p>
-
             <div className="flex flex-col items-center mb-10 xs:ml-10 ">
                 <Link to={"/Movies"}><img className="h-60 w-60 rounded-2xl bg-cyan-500" src={obj[29]}></img></Link>
                 <p className="text-2xl font-normal mt-4">Film</p>
