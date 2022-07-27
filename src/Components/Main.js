65bc101d0539f07ad2d7451f6f6d5096a8605b69
YasinAkturk <yasnaktrk@gmail.com>
2022-07-27 16:52:52 +0300

    değişiklikler geri alındı

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,16 @@
-import DiziFilm from "../../Movie.json"
+
 export default function Main() {
-    let veri=DiziFilm;
     return (
-        
+
         <div className="flex flex-wrap mt-10 xs:justify-start justify-center">
             <div className="flex flex-col items-center mb-10 xs:ml-10 ">
-                
-                    <a href=""><img className="h-60 w-60 rounded-2xl" src={veri.entries[98].images["Poster Art"].url}></img></a>
-                
+
+                <a href=""><img className="h-60 w-60 rounded-2xl bg-cyan-500" ></img></a>
                 <p className="text-2xl font-normal mt-4">Film</p>
             </div>
             <div className="flex flex-col items-center xs:ml-10">
-                
-                <a href=""><img className="h-60 w-60 rounded-2xl" src={veri.entries[0].images["Poster Art"].url}></img></a>
-                
+
+                <a href=""><img className="h-60 w-60 rounded-2xl bg-cyan-500" ></img></a>
                 <p className="text-2xl font-normal mt-4">Dizi</p>
             </div>
         </div>
