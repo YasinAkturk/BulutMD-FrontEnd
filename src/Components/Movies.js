aa24522e99b9a3d8ca289682df89dabc49375b9a
YasinAkturk <yasnaktrk@gmail.com>
2022-07-28 22:48:09 +0300

    18 veri gösterildi/fazlalık olan kodlar temizlendi

--- src/Components/Movies.js
+++ src/Components/Movies.js
@@ -1,30 +1,44 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { veri } from "../data"
-import Main from "./Main";
-export default function Movies() {
+export default function Movies(props) {
+
     const [search, setSearch] = useState("")
 
-    let filtermovie = veri.filter( (movie) => {
-            return movie.title.toLocaleLowerCase().indexOf(search) !== -1
-        }
+    let filtermovie = props.film.filter((movie) => {
+        return movie.title.toLocaleLowerCase().indexOf(search) !== -1
+    }
     )
     function handleSubmit(e) {
         e.preventDefault();
     }
+
+    useEffect(() => {
+        props.film.length=18;
+        console.log("ilk açılış")
+    },[])
     
+    /*useEffect(() => {
+        props.film.length=36;
+        console.log("Render")
+    },)*/
+
     return (
         <>
             <div className="flex flex-wrap xs:flex-nowrap justify-between ml-20 mt-10">
-                <form onSubmit={handleSubmit}>
-                    <input onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())} className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5" type="text" placeholder="Film / Dizi / Oyuncu ara"></input>
-                    <select className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
-                        <option >Sırala</option>
-                        <option >Yeniye göre sırala</option>
-                        <option >Eskiye göre sırala</option>
-                        <option >Puana göre sırala</option>
-                        <option >Rastgele sırala</option>
-                    </select>
-                </form>
+                <input onSubmit={handleSubmit}
+                    onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())}
+                    className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
+                    type="text"
+                    placeholder="Film / Dizi / Oyuncu ara">
+                </input>
+                <select className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
+                    <option >Sırala</option>
+                    <option >Yeniye göre sırala</option>
+                    <option >Eskiye göre sırala</option>
+                    <option >Puana göre sırala</option>
+                    <option >Rastgele sırala</option>
+                </select>
+
             </div>
             <div className="flex flex-wrap mt-10 xs:ml-10 xs:justify-start justify-center">
                 {
--- src/Components/Series.js
+++ src/Components/Series.js
@@ -1,36 +1,54 @@
 import { veri } from "../data"
-import { useState } from "react"
-export default function Series() {
+import { useState, useEffect } from "react"
+export default function Series(props) {
+
     const [search, setSearch] = useState("")
 
-    let filtermovie = veri.filter((movie) => {
+    let filtermovie = props.dizi.filter((movie) => {
         return movie.title.toLocaleLowerCase().indexOf(search) !== -1
     }
     )
     function handleSubmit(e) {
         e.preventDefault();
     }
+
+    useEffect(() => {
+        props.dizi.length=18;
+        console.log("ilk açılış")
+    },[])
+
+    /*useEffect(() => {
+        props.dizi.length=18;
+        console.log("Render")
+    },)*/
+
+    
+
     return (
         <>
-            <div className="flex flex-wrap xs:flex-nowrap justify-between ml-10 mt-10">
-                <form onSubmit={handleSubmit}>
-                    <input onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())} className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5" type="text" placeholder="Film / Dizi / Oyuncu ara"></input>
-                    <select className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
-                        <option >Sırala</option>
-                        <option >Yeniye göre sırala</option>
-                        <option >Eskiye göre sırala</option>
-                        <option >Puana göre sırala</option>
-                        <option >Rastgele sırala</option>
-                    </select>
-                </form>
+            <div className="flex flex-wrap xs:flex-nowrap justify-between ml-20 mt-10">
+                <input 
+                onSubmit={handleSubmit} 
+                onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())} 
+                className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
+                type="text" 
+                placeholder="Film / Dizi / Oyuncu ara">
+                </input>
+                <select className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
+                    <option >Sırala</option>
+                    <option >Yeniye göre sırala</option>
+                    <option >Eskiye göre sırala</option>
+                    <option >Puana göre sırala</option>
+                    <option >Rastgele sırala</option>
+                </select>
             </div>
             <div className="flex flex-wrap mt-10 xs:ml-10 xs:justify-start justify-center">
                 {
-                    filtermovie.map(imgurl => {
+                    filtermovie.map((series, key) => {
                         return (
-                            <div className="flex flex-col items-center mb-10 xs:ml-10 ">
-                                <img className="h-60 w-60 rounded-2xl " src={imgurl.images["Poster Art"].url}></img>,
-                                <p className="text-2xl w-60 font-normal mt-4">{imgurl.title}</p>
+                            <div key={key} className="flex flex-col items-center mb-10 xs:ml-10 ">
+                                <img className="h-60 w-60 rounded-2xl " src={series.images["Poster Art"].url}></img>,
+                                <p className="text-2xl w-60 font-normal mt-4">{series.title}</p>
                             </div>
                         )
                     })
