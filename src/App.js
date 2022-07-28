ce8664d5f1546c149ddf0777f34398d37b8de55c
YasinAkturk <yasnaktrk@gmail.com>
2022-07-28 22:45:14 +0300

    Toplu veriyi film ve dizi kategorilerine ayırdım

--- src/App.js
+++ src/App.js
@@ -5,18 +5,22 @@ import Footer from "./Components/Footer"
 import Main from "./Components/Main"
 import Movies from "./Components/Movies"
 import Series from "./Components/Series"
+import { veri } from "./data"
 
 function App() {
+
+  let movie = [], serie = [];
+  veri.map(data => (data.programType=="movie")==true?movie.push(data):serie.push(data))
+  
   return (
-    
     <div className="flex flex-col min-h-screen">
 
       <Router>
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
-          <Route path="/Movies" element={<Movies />} />
-          <Route path="/Series" element={<Series />} />
+          <Route path="/Movies" element={<Movies film={movie}/>} />
+          <Route path="/Series" element={<Series dizi={serie}/>} />
         </Routes>
         <Footer />
       </Router>
