455c22d54cf57b30b11b948b6d4639b90accc851
YasinAkturk <yasnaktrk@gmail.com>
2022-07-28 18:46:18 +0300

    günlük güncelleme

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,25 +1,28 @@
 import { Link } from "react-router-dom";
-import Movies from "../Components/Movies"
-import Series from "../Components/Series"
 import { veri } from "../data"
-export default function Main() { 
+export default function Main({movie=[], serie=[]}) {
     
-    var myObj = (veri.map(data => data.images["Poster Art"].url));
-    var myJSON = JSON.stringify(myObj);
-    localStorage.setItem("testJSON", myJSON);
-
-    var text = localStorage.getItem("testJSON");
-    var obj = JSON.parse(text);
-
+    var imagesurl = (veri.map(data => data.images["Poster Art"].url));
+    veri.forEach(show);
+    function show(item){
+        let a = Object.values(item);
+        if (a[2]=="movie"){
+            movie.push(a)
+        }
+        else{
+            serie.push(a)
+        }
+        
+    }
+    
     return (
-
         <div className="flex flex-wrap mt-10 xs:justify-start justify-center">
             <div className="flex flex-col items-center mb-10 xs:ml-10 ">
-                <Link to={"/Movies"}><img className="h-60 w-60 rounded-2xl bg-cyan-500" src={obj[29]}></img></Link>
+                <Link to={"/Movies"}><img className="h-60 w-60 rounded-2xl bg-cyan-500" src={imagesurl[0]}></img></Link>
                 <p className="text-2xl font-normal mt-4">Film</p>
             </div>
             <div className="flex flex-col items-center xs:ml-10">
-                <Link to={"/Series"}><img className="h-60 w-60 rounded-2xl bg-cyan-500" src={obj[14]}></img></Link>
+                <Link to={"/Series"}><img className="h-60 w-60 rounded-2xl bg-cyan-500" src={imagesurl[98]}></img></Link>
                 <p className="text-2xl font-normal mt-4">Dizi</p>
             </div>
         </div >
