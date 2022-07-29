2eb4f9914f5493880c3000b4fc6e4ee07619d2bb
YasinAkturk <yasnaktrk@gmail.com>
2022-07-29 23:14:08 +0300

    Filtreleme işlemi yokken 18 veri gösteriliyor. (Proje Tamamlandı)

--- src/Components/Series.js
+++ src/Components/Series.js
@@ -1,17 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function Series(props) {
     const [selected, setSelected] = useState("0")
     const [search, setSearch] = useState("")
     let filtermovie;
 
+    useEffect(() => {
+        let filterForm = document.forms["FilterForm"];
+        let searchBox = filterForm["search"];
+        if (searchBox.value == "" && selected == "") {
+            setSelected("0");
+        }
+    })
+
     function handleSubmit(e) {
         e.preventDefault();
     }
 
     filtermovie = props.dizi.filter((serie) =>
-    serie.title.toLocaleLowerCase().indexOf(search.length >= 3 ? search : "") !== -1);
-    console.log("Aranan Veri    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+        serie.title.toLocaleLowerCase().indexOf(search.length >= 3 ? search : "") !== -1);
+    //console.log("Aranan Veri    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
 
     switch (selected) {
         case "0":
@@ -19,39 +27,41 @@ export default function Series(props) {
             filtermovie.length = 18;
             break;
         case "1":
-            filtermovie.sort((a,b)=>b.releaseYear - a.releaseYear)
+            filtermovie.sort((a, b) => b.releaseYear - a.releaseYear)
             break;
         case "2":
-            filtermovie.sort((a,b)=>a.releaseYear - b.releaseYear)
+            filtermovie.sort((a, b) => a.releaseYear - b.releaseYear)
             break;
         case "3":
-            filtermovie.sort((a,b)=>a.title.localeCompare(b.title))
+            filtermovie.sort((a, b) => a.title.localeCompare(b.title))
             break;
         case "4":
-            filtermovie.sort(function(a, b){return 0.5 - Math.random()})
+            filtermovie.sort(function (a, b) { return 0.5 - Math.random() })
             break;
     }
 
     return (
         <>
             <div className="flex flex-wrap xs:flex-nowrap justify-between ml-20 mt-10">
-                <input
-                    onSubmit={handleSubmit}
-                    onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())}
-                    className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
-                    type="text"
-                    placeholder="Film / Dizi / Oyuncu ara">
-                </input>
-                <select
-                value={selected}
-                onChange={(event) => setSelected(event.target.value)} 
-                className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
-                    <option value="0" className="hidden">Sırala</option>
-                    <option value="1">Yeniye göre sırala</option>
-                    <option value="2">Eskiye göre sırala</option>
-                    <option value="3">Alfabetik sırala</option>
-                    <option value="4">Rastgele sırala</option>
-                </select>
+                <form onSubmit={handleSubmit} name="FilterForm">
+                    <input
+                        name="search"
+                        onInput={(event) => setSearch(event.target.value.toLocaleLowerCase(), setSelected(""))}
+                        className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
+                        type="text"
+                        placeholder="Film / Dizi / Oyuncu ara">
+                    </input>
+                    <select
+                        value={selected}
+                        onChange={(event) => setSelected(event.target.value)}
+                        className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
+                        <option value="0" className="hidden">Sırala</option>
+                        <option value="1">Yeniye göre sırala</option>
+                        <option value="2">Eskiye göre sırala</option>
+                        <option value="3">Alfabetik sırala</option>
+                        <option value="4">Rastgele sırala</option>
+                    </select>
+                </form>
             </div>
             <div className="flex flex-wrap mt-10 xs:ml-10 xs:justify-start justify-center">
                 {
