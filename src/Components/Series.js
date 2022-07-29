6c9fe84bdf698278870a986dbbf789ff13a166f2
YasinAkturk <yasnaktrk@gmail.com>
2022-07-29 15:21:41 +0300

    Filtreleme işlemi yokken 18 veri gösteriliyor.

--- src/Components/Series.js
+++ src/Components/Series.js
@@ -1,45 +1,54 @@
-import { veri } from "../data"
 import { useState, useEffect } from "react"
+
 export default function Series(props) {
-
+    const [selected, setSelected] = useState("0")
     const [search, setSearch] = useState("")
+    let filtermovie;
 
-    let filtermovie = props.dizi.filter((movie) => {
-        return movie.title.toLocaleLowerCase().indexOf(search.length>=3?search:"") !== -1
-    }
-    )
+    //data.releaseYear.sort((a ,b) => b - a)
     function handleSubmit(e) {
         e.preventDefault();
     }
-
-    useEffect(() => {
-        props.dizi.length=18;
-        console.log("ilk açılış")
-    },[])
-
-    /*useEffect(() => {
-        props.dizi.length=18;
-        console.log("Render")
-    },)*/
-
-    
+    filtermovie = props.dizi.filter((serie) =>
+    serie.title.toLocaleLowerCase().indexOf(search.length >= 3 ? search : "") !== -1);
+    switch (selected) {
+        case "0":
+            filtermovie = props.dizi.map(data => data);
+            filtermovie.length = 18;
+            console.log("FİLTRE YOK    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            break;
+        case "1":
+            console.log("Yeniden eskiye    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            break;
+        case "2":
+            console.log("Eskiden Yeniye    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            break;
+        case "3":
+            console.log("Puana göre    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            break;
+        case "4":
+            console.log("Ortaya Karışık    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            break;
+    }
 
     return (
         <>
             <div className="flex flex-wrap xs:flex-nowrap justify-between ml-20 mt-10">
-                <input 
-                onSubmit={handleSubmit} 
-                onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())} 
-                className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
-                type="text" 
-                placeholder="Film / Dizi / Oyuncu ara">
+                <input
+                    onSubmit={handleSubmit}
+                    onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())}
+                    className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
+                    type="text"
+                    placeholder="Film / Dizi / Oyuncu ara">
                 </input>
-                <select className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
-                    <option className="hidden">Sırala</option>
-                    <option >Yeniye göre sırala</option>
-                    <option >Eskiye göre sırala</option>
-                    <option >Puana göre sırala</option>
-                    <option >Rastgele sırala</option>
+                <select
+                onChange={(event) => setSelected(event.target.value)} 
+                className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
+                    <option value="0" className="hidden">Sırala</option>
+                    <option value="1">Yeniye göre sırala</option>
+                    <option value="2">Eskiye göre sırala</option>
+                    <option value="3">Puana göre sırala</option>
+                    <option value="4">Rastgele sırala</option>
                 </select>
             </div>
             <div className="flex flex-wrap mt-10 xs:ml-10 xs:justify-start justify-center">
