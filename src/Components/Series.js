03cedf15f68bda4b171a47b22e48edcb3450db61
YasinAkturk <yasnaktrk@gmail.com>
2022-07-29 21:39:13 +0300

    Dropdown sıralama İşlemleri tamamlandı

--- src/Components/Series.js
+++ src/Components/Series.js
@@ -1,33 +1,34 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 export default function Series(props) {
     const [selected, setSelected] = useState("0")
     const [search, setSearch] = useState("")
     let filtermovie;
 
-    //data.releaseYear.sort((a ,b) => b - a)
     function handleSubmit(e) {
         e.preventDefault();
     }
+
     filtermovie = props.dizi.filter((serie) =>
     serie.title.toLocaleLowerCase().indexOf(search.length >= 3 ? search : "") !== -1);
+    console.log("Aranan Veri    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+
     switch (selected) {
         case "0":
             filtermovie = props.dizi.map(data => data);
             filtermovie.length = 18;
-            console.log("FİLTRE YOK    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
             break;
         case "1":
-            console.log("Yeniden eskiye    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            filtermovie.sort((a,b)=>b.releaseYear - a.releaseYear)
             break;
         case "2":
-            console.log("Eskiden Yeniye    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            filtermovie.sort((a,b)=>a.releaseYear - b.releaseYear)
             break;
         case "3":
-            console.log("Puana göre    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            filtermovie.sort((a,b)=>a.title.localeCompare(b.title))
             break;
         case "4":
-            console.log("Ortaya Karışık    Film Sayısı: "+filtermovie.length+"    Selected :"+selected);
+            filtermovie.sort(function(a, b){return 0.5 - Math.random()})
             break;
     }
 
@@ -36,18 +37,19 @@ export default function Series(props) {
             <div className="flex flex-wrap xs:flex-nowrap justify-between ml-20 mt-10">
                 <input
                     onSubmit={handleSubmit}
-                    onChange={(event) => setSearch(event.target.value.toLocaleLowerCase(), setSelected())}
+                    onChange={(event) => setSearch(event.target.value.toLocaleLowerCase())}
                     className="w-56 items-center xs:w-96 h-12 border-2 border-kenarlik rounded-xl pl-4 mb-5"
                     type="text"
                     placeholder="Film / Dizi / Oyuncu ara">
                 </input>
                 <select
+                value={selected}
                 onChange={(event) => setSelected(event.target.value)} 
                 className="form-select w-40 h-12 border-2 border-kenarlik rounded-xl mr-20 ml-10 pl-2">
                     <option value="0" className="hidden">Sırala</option>
                     <option value="1">Yeniye göre sırala</option>
                     <option value="2">Eskiye göre sırala</option>
-                    <option value="3">Puana göre sırala</option>
+                    <option value="3">Alfabetik sırala</option>
                     <option value="4">Rastgele sırala</option>
                 </select>
             </div>
